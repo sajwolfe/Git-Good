a668536a2c84e7337b402bdcf14b99f5d6817465
sajwolfe <39017236+sajwolfe@users.noreply.github.com>
2018-05-05 19:11:26 -0700

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -2,7 +2,6 @@ public class helloWorld{
    public static void main(String[] args){
       String he = "Hello";
       String wo = "World";
-      int greet = h + w;
-      System.out.println(h + w + greet);
+      System.out.println(h + w);
    }
 }
